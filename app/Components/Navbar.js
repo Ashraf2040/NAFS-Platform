410ba77a0d63ff2abb7871e1730de50b451c9899
Ashraf2060 <118954369+Ashraf2060@users.noreply.github.com>
2024-08-02 09:31:40 +0300

    seventh

--- app/Components/Navbar.js
+++ app/Components/Navbar.js
@@ -4,6 +4,7 @@ import Image from 'next/image';
 
 import { useRouter } from 'next/navigation';
 import { signOut, useSession } from 'next-auth/react';
+import Link from 'next/link';
 
 
 
@@ -22,7 +23,7 @@ console.log(session)
     <nav className=" poppins  mx-auto max-w-screen-xl p-4 sm:px-8 sm:py-5 ">
       <div className="sm:flex sm:items-center sm:justify-between">
         <div className="text-center items-center w-2/5  sm:text-left flex justify-between max-h-fit">
-          <a className="flex gap-1 items-center">
+          <Link href={"/"} className="flex gap-1 items-center">
             <Image
               src="/hero-img.svg"
               alt=""
@@ -32,7 +33,7 @@ console.log(session)
             <h2 className="text-2xl font-bold flex gap-2">
               NAFS <span className="text-theme">TRAINING</span>
             </h2>
-          </a>
+          </Link>
         <div>
         {session && (
 <span className='text-xl font-medium text-white  bg-themeYellow px-4 rounded-md py-2  '>Score: {session?.user?.score}</span>
